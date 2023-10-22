32e6b1d9db3f96edec6bf590ffc99da369805147
Brittiny Filbert <britfilbert@icloud.com>
2023-10-22 13:26:54 -0700

    add login functionality

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,19 @@
 // backend/app.js
 
 const express = require("express");
-// const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug');
+const csurf = require('csurf');
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
 
-// const indexRouter = require('./routes/index');
+require('./models/User');
+require('./config/passport'); // <-- ADD THIS LINE
+const passport = require('passport'); // <-- ADD THIS LINE
 const usersRouter = require('./routes/api/users'); // update the import file path
 const tweetsRouter = require('./routes/api/tweets');
-
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -17,10 +22,54 @@ app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 // app.use(express.static(path.join(__dirname, 'public'))); // serve the static files in the public folder
+// add this line right before the CORS and CSRF configurations
+app.use(passport.initialize());
+// ADD THIS SECURITY MIDDLEWARE
+// Security Middleware
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+  }
+  
+app.use(
+    csurf({
+        cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+        }
+    })
+);
 
 // Attach Express routers
 // app.use('/', indexRouter);
 app.use('/api/users', usersRouter); // update the path
 app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+  
+  const serverErrorLogger = debug('backend:error');
+  
+  // Express custom error handler that will be called whenever a route handler or
+  // middleware throws an error or invokes the `next` function with a truthy value
+  app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+  });
 
 module.exports = app;
